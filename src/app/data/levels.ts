19113ff664751906188de0f2a1d787b9f0455438
Jasper <jaspervermeulen@icloud.com>
2025-09-28 13:01:56 +0200

    chore: add starting point

---
+++ src/app/data/levels.ts
@@ -0,0 +1,23 @@
+export const levels = [
+  {
+    id: 1,
+    name: 'Light',
+    description:
+      'Voor leerlingen van 10 tot 12 jaar op zoek om eens te proeven van een job-ervaring in verschillende sectoren. Vol spelplezier en handige tips.',
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: 'Medium',
+    description:
+      'Voor leerlingen van 12 tot 14 jaar die al wat meer willen weten over de verschillende beroepen en hun interesses verder willen ontdekken.',
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: 'Heavy',
+    description:
+      'Voor leerlingen van 14 tot 16 jaar die een beter beeld willen krijgen van de arbeidsmarkt en hun toekomstige carrière willen plannen.',
+    isActive: false,
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <p>Leapforward Technisch Testje</p>;
+import LevelCard from '@/components/game/LevelCard';
+import { levels } from './data/levels';
+
+export default function HomePage() {
+  return (
+    <main className="bg-primary-blue py-32 font-onest min-h-screen flex flex-col items-center justify-center px-4">
+      <header className="text-center text-white space-y-4">
+        <h1 className="text-6xl font-bold">Kies je niveau!</h1>
+        <p className="text-lg">Welkom in Kansstad:</p>
+        <p className="max-w-2xl mx-auto text-balance text-lg">
+          Een virtueel spel waarin je kennis kan opdoen rond heel wat
+          interessante beroepen. Begin er meteen aan.
+        </p>
+      </header>
+      <section className="mt-12 md:mt-32 max-w-6xl w-full grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {levels.map(({ id, ...props }) => (
+          <LevelCard key={id} {...props} />
+        ))}
+      </section>
+    </main>
+  );
 }
