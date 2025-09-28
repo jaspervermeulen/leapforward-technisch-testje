28311dc212a4c1dce00560aa27d6b2632a28067d
Jasper <jaspervermeulen@icloud.com>
2025-09-28 13:02:47 +0200

    chore: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,18 @@
+import { Metadata } from 'next';
 import './globals.css';
+import localFont from 'next/font/local';
+
+const onest = localFont({
+  src: './onest.ttf',
+  display: 'swap',
+  variable: '--font-onest',
+});
+
+export const metadata: Metadata = {
+  title: 'Kansstad - Leer over beroepen door te spelen',
+  description:
+    'Welkom in Kansstad: Een virtueel spel waarin je kennis kan opdoen rond heel wat interessante beroepen. Begin er meteen aan.',
+};
 
 export default function RootLayout({
   children,
@@ -6,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${onest.variable} antialiased`}>
       <body>{children}</body>
     </html>
   );
